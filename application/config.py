5ac05485710205ed25426d8f2378143a5b24dc1e
Vinay Patel <w1572032@westminster.ac.uk>
2019-03-15 15:45:47 +0000

    Trained on Accompaniment

--- application/config.py
+++ application/config.py
@@ -12,7 +12,7 @@ SECRET_KEY = "b'\x89b=|\xbb\x99\x0e\x0f\xd4\xa7\x06\x90\xd0\x80&j'"
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
 AUDIO_DIR = os.path.join(ROOT, 'audio/')
-MODEL_PATH = os.path.join(ROOT, 'controllers/models/test1.pt')
+MODEL_PATH = os.path.join(ROOT, 'controllers/models/test2_2019-03-15_14-41_LSTM_accompaniment_B16_H256_S1000.pt')
 
 SUPPORTED_EXTENSIONS = [".aac",
                         ".aif",
