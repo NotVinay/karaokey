044a85f2a0faae3d47c10976a66f54d1c2fe10ab
Vinay Patel <w1572032@westminster.ac.uk>
2019-03-18 19:37:56 +0000

    Fixed Form submission error and fixed error messages

--- application/static/js/home.js
+++ application/static/js/home.js
@@ -5,6 +5,7 @@
 $( document ).ready(function() {
     var file
 
+    // capturing file on change
     $('#music').on('change', function(event) {
         $('#box-error').hide();
         file = event.target.files[0]
@@ -13,49 +14,64 @@ $( document ).ready(function() {
         }
     });
 
+    // hiding error if consent is accepted.
     $('#consent').on('change', function(event) {
-        $('#consent-error').hide()
+        if($('#consent').is(":checked")){
+            $('#consent-error').hide()
+        } else {
+            $('#consent-error').show()
+        }
     });
 
     $('#upload').on('click', function() {
-
         event.stopPropagation(); // stop executing futher events
         event.preventDefault(); // stop current event
-        console.log($('#consent').val())
+        console.log("Upload Clicked")
         if(!$('#consent').is(":checked")){
+            // showing consent error if consent is not clicked
             $('#consent-error').show()
+        } else {
+            $('#file-form').prop('disabled', true);
+            $('#upload').attr("disabled", "disabled");
+            // capturing form data
+            var data = new FormData();
+            data.append('music', file)
+            // sending ajax request for separation
+            $.ajax({
+                url: '/separate',
+                type: 'POST',
+                data: data,
+                cache: false,
+                dataType: 'json',
+                processData: false, // Stop's file process
+                contentType: false, // Stop file processing
+                success: function(data, textStatus, jqXHR)
+                {
+                    if(data.token) {
+                        // redirect to the results if successfully separated
+                        window.location.replace("/results");
+                    }
+                    if(data.error) {
+                        // show error if separation fails
+                        $('#file-form').prop('disabled', false);
+                        $('#upload').removeAttr("disabled");
+                        $('#box-error').text(data.error)
+                        $('#box-error').addClass("");
+                        $('#box-error').show();
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown)
+                {
+                    if(data.error) {
+                        // show error if separation fails
+                        $('#file-form').prop('disabled', false);
+                        $('#upload').removeAttr("disabled");
+                        $('#box-error').text(errorThrown)
+                        $('#box-error').addClass("");
+                        $('#box-error').show();
+                    }
+                }
+            });
         }
-        $('#file-form').prop('disabled', true);
-        var data = new FormData();
-        data.append('music', file)
-
-        $.ajax({
-            url: '/separate',
-            type: 'POST',
-            data: data,
-            cache: false,
-            dataType: 'json',
-            processData: false, // Don't process the files
-            contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-            success: function(data, textStatus, jqXHR)
-            {
-                if(data.token) {
-                    // redirect to the results page
-                    window.location.replace("/results");
-                }
-                if(data.error) {
-                    // show error
-                    $('#file-form').prop('disabled', false);
-                    $('#box-error').text(data.error)
-                    $('#box-error').addClass("");
-                    $('#box-error').show();
-                }
-            },
-            error: function(jqXHR, textStatus, errorThrown)
-            {
-
-                $('#file-form').prop('disabled', false);
-            }
-        });
     });
 });
